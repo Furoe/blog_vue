1d29ff7a12b0519eec879532882f23132aa02f9c
furoe <17751038730@163.com>
2020-10-21 12:47:58 +0800

    调整

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
             'vue$': 'vue/dist/vue.esm.js',
             '@': path.resolve(__dirname, './src')
         },
-        extensions: ['.ts', '.vue', '.js', '.json']
+        extensions: ['.ts', '.vue', '.js', '.json', '.less']
     },
     module: {
         rules: [{
