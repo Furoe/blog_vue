1564e8561c8cfe7d7db0b8ad0a3546ea23134b15
furoe <17751038730@163.com>
2020-10-25 14:53:41 +0800

    fxit

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin');
 module.exports = {
     //项目入口文件
     entry: {
-        app: ['babel/polyfill', './src/main.ts']
+        app: ['./node_modules/babel-polyfill', './src/main.ts']
     },
     output: {
         //打包出口路径
