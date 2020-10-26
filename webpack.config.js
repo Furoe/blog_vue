c10b0c776b2adb62e478021461b15b0b59889951
furoe <17751038730@163.com>
2020-10-26 08:25:47 +0800

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -62,7 +62,7 @@ module.exports = {
         },{
             test: /\.ts$/,
             loader: 'ts-loader',
-            exclude: '/node_modules/',
+            exclude: /node_modules/,
             options: {
                 appendTsSuffixTo: [/\.vue$/]
             }
